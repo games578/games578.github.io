8b399a2176ece03b3de47fcd9244f2cbb67c3148
games578 <vladislavpilipchuk11@gmail.com>
2026-03-10 21:37:30 +0000

    Add files via upload

---
+++ games.js
@@ -0,0 +1,57 @@
+const games = [
+  {
+    id: "a-small-world-cup",
+    title: "A Small World Cup",
+    src: "https://a-small-world-cup.vercel.app/file/"
+  },
+    {
+    id: "subway-surfers",
+    title: "Subway Surfers",
+    src: "https://jamestore214.github.io/g26/class-444/"
+  },
+  {
+    id: "murder",
+    title: "Murder",
+    src: "https://jamestore214.github.io/g72/class-580/"
+  },
+    {
+    id: "slope",
+    title: "Slope",
+    src: "https://jamestore214.github.io/g5/class-450"
+  },
+    {
+    id: "swat",
+    title: "Swat",
+    src: "blob:https://1698041343-atari-embeds.googleusercontent.com/956647dc-0dd0-4a32-babf-cbffd09bbc2d"
+  },
+      {
+    id: "lol",
+    title: "lol",
+    src: "https://mc0825.github.io/g20/class-440/"
+  },
+  {
+    id: "snow-rider",
+    title: "Snow Rider",
+    src: "https://n-umwhousjvethbvlcgmicfvstdxy2skj3gwmb3rq-0lu-script.googleusercontent.com/userCodeAppPanel"
+  },
+  {
+    id: "shotz",
+    title: "Shotz",
+    src: "https://mc0825.github.io/g22/class-394/"
+  },
+    {
+    id: "goalkeeper-challenge",
+    title: "Goalkeeper Challenge",
+    src: " https://mc0825.github.io/g68/class-1049/"
+  },
+      {
+    id: "stair-race",
+    title: "Stair Race 3D",
+    src: " https://mc0825.github.io/g2/class-619/"
+  },
+        {
+    id: "among-us",
+    title: "Among Us",
+    src: "https://mc0825.github.io/g5/class-468/"
+  },
+]; 
